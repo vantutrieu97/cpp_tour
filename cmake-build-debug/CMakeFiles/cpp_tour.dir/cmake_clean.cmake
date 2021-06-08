ceeace36c0204a07a3a84270c6769fc433528e65
vantutrieu97 <vantutrieu97@gmail.com>
2021-06-08 18:08:01 +0700

    Hello world

---
+++ cmake-build-debug/CMakeFiles/cpp_tour.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_tour.dir/main.cpp.o"
+  "cpp_tour"
+  "cpp_tour.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_tour.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
